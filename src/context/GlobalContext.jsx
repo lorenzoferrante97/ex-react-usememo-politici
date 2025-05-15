f8fdf4ae0615b13822b1c023a6921dcc15dae083
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-15 21:21:48 +0200

    add: fetch users

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -5,7 +5,20 @@ import { createContext, useState, useContext } from 'react';
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-  const value = {};
+  const [users, setUsers] = useState([]);
+
+  // fetch users
+  const getUsers = async () => {
+    try {
+      const fetchUsers = await fetch('https://dummyjson.com/users?limit=6');
+      const usersJson = await fetchUsers.json();
+      return usersJson;
+    } catch (err) {
+      throw new Error('Failed to get Users with fetch');
+    }
+  };
+
+  const value = { getUsers, setUsers, users };
 
   return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;
 };
